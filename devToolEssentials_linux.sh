d6f994e43eda4b8df1530af74346cac90c7787f4
Ben Clouser <bclouse91@gmail.com>
2018-02-11 14:36:18 -0800

    Some config changes. Adding in silver searcher install into dev install scripts

--- devToolEssentials_linux.sh
+++ devToolEssentials_linux.sh
@@ -15,4 +15,5 @@ function installEssentialApps {
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo apt-get update
 	sudo apt-get install sublime-text
+	sudo apt-get install silversearcher-ag
 }
--- devToolEssentials_mac.sh
+++ devToolEssentials_mac.sh
@@ -9,4 +9,6 @@ function installEssentialApps {
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || {
 		echo "Installing brew seems to have failed"
 	}
+
+	brew install the_silver_searcher
 }
